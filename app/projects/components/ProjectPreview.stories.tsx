10d6def2d2c427632e685b94cf616e0878de9a79
GersonEC <gersonenriquez94@gmail.com>
2021-12-18 14:04:46 +0100

    Clear files.

--- app/projects/components/ProjectPreview.stories.tsx
+++ app/projects/components/ProjectPreview.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { ComponentStory, ComponentMeta } from "@storybook/react"
-import { ProjectPreview } from "../app/projects/components/ProjectPreview"
+import { ProjectPreview } from "./ProjectPreview"
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
